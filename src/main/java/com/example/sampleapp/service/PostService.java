afab4dce4fc2a87e45acf8ebcd1490f61dd08260
Yuuki Ooguro <kitkat.jaja@gmail.com>
2019-06-26 14:32:06 +0900

    投稿日時にタイムゾーンを考慮

--- src/main/java/com/example/sampleapp/service/PostService.java
+++ src/main/java/com/example/sampleapp/service/PostService.java
@@ -3,7 +3,8 @@ package com.example.sampleapp.service;
 import com.example.sampleapp.domain.model.Post;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,7 +13,7 @@ public class PostService {
     List<Post> posts = new ArrayList<>();
     
     public void add(String title, String message) {
-        Post post = new Post(title, message, LocalDateTime.now());
+        Post post = new Post(title, message, OffsetDateTime.now(ZoneId.of("Asia/Tokyo")).toLocalDateTime());
         posts.add(post);
     }
     
